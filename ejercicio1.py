1f0b5ec47f322359b8e2b58f5e8298757a089d1e
Franchu95 <franchubernal@gmail.com>
2016-11-03 20:16:59 +0100

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+def sumli(li):
+	suma = 0
+	for ele in li:
+		if (ele > 0):
+			suma += ele
+	return suma
+	
---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+def impar(li2):
+	lif=[]
+	for ele in li2:
+		if ele % 2 != 0:
+			lif.append(ele)
+	return lif
+			
+		
+		
+	
